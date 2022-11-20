91271bdb5b41032f69403d5de817a1bc8e175503
Hosna Qasmei <hosna.qasmei@gmail.com>
2022-11-19 18:47:29 -0800

    Added to snippets of wisdom

--- src/components/ResourcesSection/SnippetsOfWisdom/Data.jsx
+++ src/components/ResourcesSection/SnippetsOfWisdom/Data.jsx
@@ -1,58 +1,58 @@
-export const favoritePodcasts = [
-  {
-    title: "10 Ways To Spot Ego & How to Replace it With ",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5xZBPMNPciYn3eLJ0qjXUB?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "6 Things To Do If Your Job Is Harming Your Mental Health",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1LrluzQ0NYv4Csig3bW76a?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "A New Earth: Awakening to Your Life's Purpose",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0eNp2s0OsBsYjeQiyIIjU0?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "A New Earth: The Pain Body",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5QAdmUXyn0wI8ArW8KImWK?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "Brene with Abby Wambach on the New Rules of Leadership",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0zRdwoqoMMYqb0ZsEEJRON?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "Brene with Brandi Carlile on Music, Mysticism, and Broken Horses",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5Lzryk7lOVEPN3II6gHKXH?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title:
-      "Brene with Susan Cain on How Sorrow and Longing Make Us Whole, Part 1 of 2",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2y2vlxXQpE7pQcNl12nxId?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title:
-      "Harriet Lerner and Brene - I'm Sorry: How to Apologize & Why It Matters, Part 1 of 2",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2jxcLXMB672u5dpc9YqDu9?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "How to Say No: Boundaires with Nedra Glover Tawwab",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5tg8x6CNdQiK7dVlQSZxF1?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "Liz Gilbert Shares the Whole Story for the First Time",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1FtQHtJ3PevbSTJQn1AcUF?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "Our Most Embarrassing Stories",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/7HLkIDhTfU03eugSLU0qNn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-  {
-    title: "Susan Cain Says Sadness is a Superpower",
-    link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/7a03c9bYoj5jKb33WimTES?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
-  },
-]
+// export const favoritePodcasts = [
+//   {
+//     title: "10 Ways To Spot Ego & How to Replace it With ",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5xZBPMNPciYn3eLJ0qjXUB?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "6 Things To Do If Your Job Is Harming Your Mental Health",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1LrluzQ0NYv4Csig3bW76a?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "A New Earth: Awakening to Your Life's Purpose",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0eNp2s0OsBsYjeQiyIIjU0?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "A New Earth: The Pain Body",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5QAdmUXyn0wI8ArW8KImWK?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "Brene with Abby Wambach on the New Rules of Leadership",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0zRdwoqoMMYqb0ZsEEJRON?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "Brene with Brandi Carlile on Music, Mysticism, and Broken Horses",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5Lzryk7lOVEPN3II6gHKXH?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title:
+//       "Brene with Susan Cain on How Sorrow and Longing Make Us Whole, Part 1 of 2",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2y2vlxXQpE7pQcNl12nxId?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title:
+//       "Harriet Lerner and Brene - I'm Sorry: How to Apologize & Why It Matters, Part 1 of 2",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2jxcLXMB672u5dpc9YqDu9?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "How to Say No: Boundaires with Nedra Glover Tawwab",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5tg8x6CNdQiK7dVlQSZxF1?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "Liz Gilbert Shares the Whole Story for the First Time",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1FtQHtJ3PevbSTJQn1AcUF?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "Our Most Embarrassing Stories",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/7HLkIDhTfU03eugSLU0qNn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+//   {
+//     title: "Susan Cain Says Sadness is a Superpower",
+//     link: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/7a03c9bYoj5jKb33WimTES?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+//   },
+// ]
 
 export const podcasts = [
-  ({
+  {
     title: "Dare to Lead with Brene Brown",
     speaker: "Brene Brown",
     link: "https://open.spotify.com/show/3oEPsPKDhPVoNNL7pH5db6?si=d8f9286e40a240db",
@@ -96,12 +96,12 @@ export const podcasts = [
     title: "We Can Do Hard Things with Glennon Doyle",
     speaker: "Glennon Doyle",
     link: "https://open.spotify.com/show/0eFL5HJejQHZrdgAFdPnOm?si=7057d2acc2b44eed",
-  }),
+  },
 ]
 
 export const tedTalks = [
-  ({
-    title: '"A Seat at the Table" Isn\'t the Solution for Gender Equity\u00a0',
+  {
+    title: "A Seat at the Table Isnt the Solution for Gender Equity",
     speaker: "Lilly Singh",
     link: "https://youtu.be/9EBkS2kE7uk",
   },
@@ -129,5 +129,5 @@ export const tedTalks = [
     title: "Why 30 is not the new 20",
     speaker: "Meg Jay",
     link: "https://youtu.be/vhhgI4tSMwc",
-  }),
+  },
 ]
--- src/components/ResourcesSection/SnippetsOfWisdom/SnippetsOfWisdom.jsx
+++ src/components/ResourcesSection/SnippetsOfWisdom/SnippetsOfWisdom.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Container } from "react-bootstrap"
+import { tedTalks, podcasts, favoritePodcasts } from "./Data"
 import { BreadCrumb } from "../../BreadCrumb/BreadCrumb"
 import "./SnippetsOfWisdom.css"
 
@@ -10,6 +11,30 @@ const SnippetsOfWisdom = () => {
         <BreadCrumb />
         <Container>
           <h1>Snippets Of Wisdom</h1>
+          <p>
+            Here are a complelation of TED talks, authors, articles, videos,
+            podcasts, and quotes that I like to reference time to time.
+          </p>
+          <h3>Ted Talks</h3>
+          {tedTalks.map((val, idx) => (
+            <li key={idx}>
+              <a target="_blank" rel="noopener noreferrer" href={val.link}>
+                {val.title}
+              </a>
+              , {val.speaker}
+            </li>
+          ))}
+          <h3>Podcasts</h3>
+          {podcasts.map((val, idx) => (
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href={val.link}>
+                {val.title}
+              </a>
+              , {val.speaker}
+            </li>
+          ))}
+          *** my favorite episodes can be found{" "}
+          {/* <a href="favorite_podcast_episodes.html">here</a>. */}
         </Container>
       </div>
     </>
