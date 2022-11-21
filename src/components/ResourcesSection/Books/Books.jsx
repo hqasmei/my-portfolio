395d9a0bddea4ab2c1c76b34b574f3d40b26ccea
Hosna Qasmei <hosna.qasmei@gmail.com>
2022-11-20 23:53:18 -0800

    WIP fixing pagination bug

--- src/components/ResourcesSection/Books/Books.jsx
+++ src/components/ResourcesSection/Books/Books.jsx
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from "react"
 import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
+import { Layout, Pagination } from "antd"
 import { Container } from "react-bootstrap"
 import { BreadCrumb } from "../../BreadCrumb/BreadCrumb"
-// import { bookList } from "./Data"
 import "./Books.css"
+
+const { Content } = Layout
 
 const Books = () => {
   const [data, setData] = useState([])
+  const [total, setTotal] = useState()
+  const [page, setPage] = useState(0)
+  const [postPerPage, setPostPerPage] = useState(10)
 
   useEffect(() => {
     loadData()
+    setTotal(75)
   }, [])
+
+  const indexOfLastPage = page + postPerPage
+  const indexOfFirstPage = indexOfLastPage - postPerPage
+  const currentData = data.slice(indexOfFirstPage, indexOfLastPage)
+
+  const onShowSizeChange = (current, pageSize) => {
+    setPostPerPage(pageSize)
+  }
 
   const loadData = async () => {
     await Promise.all([
@@ -38,19 +52,16 @@ const Books = () => {
         collectedData.forEach(function (d) {
           d.items.forEach((i) => {
             setData((data) => [...data, i])
-            console.log(i)
           })
         })
+
+        
       })
       .catch(function (error) {
         // if there's an error, log it
         console.log(error)
       })
   }
-  // this.rawRequests.push(...response.data)
-  // .then((responses) => {
-  //         responses.forEach((response) => console.log(response))
-  //       })
   return (
     <>
       <div className="books-section books-darkBg">
@@ -61,22 +72,39 @@ const Books = () => {
             Here is a list of 90+ books that I have read that can be filtered by
             subject.
           </p>
-          <Row xs={2} md={3} lg={4} className="g-4">
-            {data.map((val, idx) => (
-              <Col key={idx}>
-                <Card key={val.id} className="card-text">
-                  <Card.Img
-                    variant="top"
-                    src={val.volumeInfo.imageLinks.smallThumbnail}
-                  />
-                  <Card.Header>{val.volumeInfo.title}</Card.Header>
-                  <Card.Body>
-                    <Card.Text>{val.volumeInfo.authors[0]}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+          <Layout className="layout">
+            <Content
+              style={{
+                padding: "0 50px",
+              }}
+            >
+              <div className="site-layout-content">
+                <Row xs={2} md={3} lg={4} className="g-4">
+                  {currentData.map((val, idx) => (
+                    <Col key={idx}>
+                      <Card key={idx} className="card-text">
+                        <Card.Img
+                          variant="top"
+                          src={val.volumeInfo.imageLinks.smallThumbnail}
+                        />
+                        <Card.Header>{val.volumeInfo.title}</Card.Header>
+                        <Card.Body>
+                          <Card.Text>{val.volumeInfo.authors[0]}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+                </Row>
+                <Pagination
+                  onChange={(value) => setPage(value)}
+                  pageSize={postPerPage}
+                  total={total}
+                  current={page}
+                  onShowSizeChange={onShowSizeChange}
+                />
+              </div>
+            </Content>
+          </Layout>
         </Container>
       </div>
     </>
