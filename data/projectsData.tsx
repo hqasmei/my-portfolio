dd1bc7ec7fb0267d858c0a4c180d4174cb9b0a9b
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-01-22 23:49:30 -0800

    Added ThankfulThoughts to project

--- data/projectsData.tsx
+++ data/projectsData.tsx
@@ -1,13 +1,23 @@
 export const projectsData = [
   {
-    name: "SportsTable", 
+    name: "ThankfulThoughts",
+    description:
+      "ThankfulThoughts is a web app that generates an appreciative sentence of something or someone you are thankful for.",
+    image: "/thankfulthoughts-image.png",
+    link: "https://thankful-thoughts-hqasmei.vercel.app/",
+    github: "https://github.com/hqasmei/thankful-thoughts",
+    tech: ["Next.js", "Vercel", "Open AI API"],
+    reverse: false,
+  },
+  {
+    name: "SportsTable",
     description:
       "SportsTable is a web app that localizes sports highlights in one place.",
-    image: "/sportstable-image.png", 
+    image: "/sportstable-image.png",
     link: "https://sportstable.xyz/",
     github: "https://github.com/hqasmei/sportstable",
     tech: ["React", "Express", "Postgres"],
-    reverse: false,
+    reverse: true,
   },
 
   {
@@ -17,17 +27,16 @@ export const projectsData = [
     link: "https://platoio.com/register",
     github: "https://github.com/hqasmei/platoio",
     tech: ["React", "Express", "Postgres"],
-    reverse: true,
+    reverse: false,
   },
   {
     name: "Kator Family Photos",
     description:
       "Kator Family Photos is a photos and video digitization service in the LA area.",
-    image:
-      "/katorfamilyphotos-image.png",
+    image: "/katorfamilyphotos-image.png",
     github: "https://github.com/hqasmei/katorfamilyphotos",
     link: "https://katorfamilyphotos.com/",
     tech: ["React", "Tailwind CSS", "Chakra UI"],
-    reverse: false,
+    reverse: true,
   },
 ]
