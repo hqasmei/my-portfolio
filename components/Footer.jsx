616a22aad6eb9973a2195a6766ff53cd04b1a455
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-03 23:09:30 -0800

    Fixed noreferrer error

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,11 @@ const Footer = () => {
       <div className="flex h-16 flex-col items-center justify-center md:hidden">
         <ul className="flex flex-row items-center space-x-6">
           <li>
-            <a href="https://github.com/hqasmei" target="_blank">
+            <a
+              href="https://github.com/hqasmei"
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiOutlineGithub
                 className="cursor-pointer text-white  transition-transform hover:-translate-y-1 "
                 size={25}
@@ -20,7 +24,11 @@ const Footer = () => {
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/hqasmei" target="_blank">
+            <a
+              href="https://twitter.com/hqasmei"
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiOutlineTwitter
                 className="cursor-pointer text-white transition-transform hover:-translate-y-1 "
                 size={25}
@@ -28,7 +36,11 @@ const Footer = () => {
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/hosnaqasmei/" target="_blank">
+            <a
+              href="https://www.linkedin.com/in/hosnaqasmei/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiOutlineLinkedin
                 className="cursor-pointer text-white  transition-transform hover:-translate-y-1"
                 size={25}
@@ -39,6 +51,7 @@ const Footer = () => {
             <a
               href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
               target="_blank"
+              rel="noreferrer"
             >
               <AiOutlineYoutube
                 className="cursor-pointer text-white transition-transform hover:-translate-y-1 "
--- components/Social.jsx
+++ components/Social.jsx
@@ -11,7 +11,7 @@ const Social = () => {
     <div className="hidden md:fixed md:bottom-0 md:z-10 md:block md:w-32 ">
       <ul className=" flex flex-col items-center space-y-6 after:block after:h-28 after:w-0.5 after:bg-white">
         <li>
-          <a href="https://github.com/hqasmei" target="_blank">
+          <a href="https://github.com/hqasmei" target="_blank" rel="noreferrer">
             <AiOutlineGithub
               className="cursor-pointer text-white transition-transform hover:-translate-y-1 "
               size={25}
@@ -19,7 +19,11 @@ const Social = () => {
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/hqasmei" target="_blank">
+          <a
+            href="https://twitter.com/hqasmei"
+            target="_blank"
+            rel="noreferrer"
+          >
             <AiOutlineTwitter
               className="cursor-pointer text-white transition-transform hover:-translate-y-1"
               size={25}
@@ -27,7 +31,11 @@ const Social = () => {
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/hosnaqasmei/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/hosnaqasmei/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <AiOutlineLinkedin
               className="cursor-pointer text-white transition-transform hover:-translate-y-1 "
               size={25}
@@ -38,6 +46,7 @@ const Social = () => {
           <a
             href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
             target="_blank"
+            rel="noreferrer"
           >
             <AiOutlineYoutube
               className="cursor-pointer text-white transition-transform hover:-translate-y-1 "
