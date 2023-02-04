a12a787ddc5fa546fb2952eccecd6f2eade27970
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-03 23:02:27 -0800

    Re positioned home text left

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="mx-auto min-h-screen text-center">
+    <main className="mx-auto min-h-screen text-left">
       <div
-        className="px-8 py-64 sm:px-24 md:px-28
+        className="px-8 py-64 sm:px-28 md:px-64
       md:py-52"
       >
         <h1 className="pb-4 text-4xl font-bold text-white md:text-6xl">
