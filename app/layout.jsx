4c911a8a6e68a85800a465508c154f436bdfb03d
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-05 22:28:41 -0800

    Added splash screen

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,23 @@
 "use client"
 import "../styles/globals.css"
+import React, { useState, useEffect } from "react"
 import { Navbar } from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import Social from "@/components/Social"
+import { usePathname } from "next/navigation"
+import SplashScreen from "@/components/SplashScreen"
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname()
+  const isHome = pathname === "/"
+  const [isLoading, setIsLoading] = useState(isHome)
+
+  useEffect(() => {
+    if (isLoading) {
+      return
+    }
+  }, [isLoading])
+
   return (
     <html lang="en">
       {/*
@@ -13,10 +26,16 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body className="bg-fill bg-[url('/background.png')] bg-fixed bg-no-repeat">
-        <Navbar />
-        <Social />
-        {children}
-        <Footer />
+        {isLoading && isHome ? (
+          <SplashScreen finishLoading={() => setIsLoading(false)} />
+        ) : (
+          <>
+            <Navbar />
+            <Social />
+            {children}
+            <Footer />
+          </>
+        )}
       </body>
     </html>
   )
