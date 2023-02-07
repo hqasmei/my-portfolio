448f6f8050970fcb9fa72bd93f2ad53a8ec0c874
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-06 22:06:39 -0800

    Added more resources

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -1,100 +1,8 @@
 import React from "react"
-
-const resources = [
-  {
-    name: "goodbrief",
-    description: "A random generator for design briefs.",
-    link: "https://goodbrief.io/",
-  },
-  {
-    name: "Midjourney",
-    description: "AI Text to Image Generator.",
-    link: "https://midjourney.com/home/?callbackUrl=%2Fapp%2F",
-  },
-  {
-    name: "freepik",
-    description: "Image bank website.",
-    link: "https://www.freepik.com/free-photos-vectors/flat-illustration",
-  },
-  {
-    name: "removebg",
-    description: "Free Background Remover.",
-    link: "https://www.remove.bg/",
-  },
-  {
-    name: "favicon.io",
-    description: "Free favicon generator.",
-    link: "https://favicon.io/",
-  },
-  {
-    name: "React Icons",
-    description: "Popular icon packs.",
-    link: "https://react-icons.github.io/react-icons",
-  },
-  {
-    name: "heroicons",
-    description: "Icons from Tailwind CSS.",
-    link: "https://heroicons.com/",
-  },
-  {
-    name: "Flowbite",
-    description: "Components using Tailwind CSS.",
-    link: "https://flowbite.com/",
-  },
-  {
-    name: "Vercel",
-    description: "Easy deployment platform.",
-    link: "https://vercel.com/",
-  },
-  {
-    name: "PlanetScale",
-    description: "Serverless MySQL  platform.",
-    link: "https://planetscale.com/",
-  },
-  {
-    name: "umami",
-    description: "Alternative to Google Analytics.",
-    link: "https://umami.is/",
-  },
-  {
-    name: "Rapid API",
-    description: "API Hub.",
-    link: "https://rapidapi.com/hub",
-  },
-  {
-    name: "Excalidraw",
-    description: "Virtual collaborative whiteboard.",
-    link: "https://excalidraw.com/",
-  },
-  {
-    name: "Craft",
-    description: "Alternative to Notion.",
-    link: "https://www.craft.do/",
-  },
-  {
-    name: "Futurepedia.io",
-    description: "AI tool directory.",
-    link: "https://www.futurepedia.io/",
-  },
-  {
-    name: "Tailwind CSS",
-    description: "Utility-first CSS framework.",
-    link: "https://tailwindcss.com/",
-  },
-  {
-    name: "Product Hunt",
-    description: "Curation of the best new products.",
-    link: "https://www.producthunt.com/",
-  },
-  {
-    name: "Gumroad",
-    description: "E-commerce platform.",
-    link: "https://gumroad.com/",
-  },
-]
+import { RESOURCES } from "../../data.js"
 
 export default function ResourcesPage() {
-  const sortedArr = resources.sort((a, b) => a.name.localeCompare(b.name))
+  const sortedArr = RESOURCES.sort((a, b) => a.section.localeCompare(b.section))
   return (
     <main className="mx-auto min-h-screen max-w-3xl px-6  md:max-w-5xl">
       <div className="py-24 sm:px-28 sm:py-28">
@@ -106,22 +14,38 @@ export default function ResourcesPage() {
           reference frequently and find very useful. It's a work in progress.
         </p>
 
-        <ul className="text-neutral-100">
-          {sortedArr.map((item, idx) => {
-            return (
-              <li key={idx}>
-                <p>
-                  {" "}
-                  <a href={item.link} target="_blank" className="underline">
-                    {item.name}
-                  </a>
-                  {" - "}
-                  {item.description}
-                </p>
-              </li>
-            )
-          })}
-        </ul>
+        {sortedArr.map((item, idx) => {
+          const sortedSubArr = item.data.sort((a, b) =>
+            a.name.localeCompare(b.name)
+          )
+          return (
+            <div key={idx} className="mb-4">
+              <h1 className="text-2xl font-semibold text-neutral-100">
+                {item.section}
+              </h1>
+              <ul className="ml-8 list-disc text-neutral-100 ">
+                {sortedSubArr.map((i, i_idx) => {
+                  return (
+                    <li key={i_idx} className="">
+                      <p>
+                        {" "}
+                        <a
+                          href={i.link}
+                          target="_blank"
+                          className="underline hover:text-neutral-400"
+                        >
+                          {i.name}
+                        </a>
+                        {" - "}
+                        {i.description}
+                      </p>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })}
       </div>
     </main>
   )
---
+++ data.js
@@ -0,0 +1,150 @@
+export const RESOURCES = [
+  {
+    section: "Cloud Platforms",
+    data: [
+      {
+        name: "PlanetScale",
+        description: "Serverless MySQL platform.",
+        link: "https://planetscale.com/",
+      },
+      {
+        name: "Supabase",
+        description: "An open source Firebase alternative.",
+        link: "https://supabase.com/",
+      },
+      {
+        name: "Netlify",
+        description: "A serverless development platform.",
+        link: " https://www.netlify.com/",
+      },
+
+      {
+        name: "Vercel",
+        description: "Easy deployment platform.",
+        link: "https://vercel.com/",
+      },
+
+      {
+        name: "Rapid API",
+        description: "API Hub.",
+        link: "https://rapidapi.com/hub",
+      },
+    ],
+  },
+  {
+    section: "Design Tools",
+    data: [
+      {
+        name: "freepik",
+        description: "Image bank website.",
+        link: "https://www.freepik.com/free-photos-vectors/flat-illustration",
+      },
+      {
+        name: "removebg",
+        description: "Free Background Remover.",
+        link: "https://www.remove.bg/",
+      },
+      {
+        name: "favicon.io",
+        description: "Free favicon generator.",
+        link: "https://favicon.io/",
+      },
+      {
+        name: "Figma",
+        description: "Collaborative web application for interface design.",
+        link: "https://www.figma.com/",
+      },
+      {
+        name: "Canva",
+        description: "A graphic design platform.",
+        link: "https://www.canva.com/",
+      },
+    ],
+  },
+  {
+    section: "AI Tools",
+    data: [
+      {
+        name: "Midjourney",
+        description: "AI Text to Image Generator.",
+        link: "https://midjourney.com/home/?callbackUrl=%2Fapp%2F",
+      },
+      {
+        name: "ChatGPT",
+        description: "OpenAI chatbot that uses GPT-3.",
+        link: " https://chat.openai.com/auth/loginF",
+      },
+    ],
+  },
+  {
+    section: "Miscellaneous",
+    data: [
+      {
+        name: "goodbrief",
+        description: "A random generator for design briefs.",
+        link: "https://goodbrief.io/",
+      },
+      {
+        name: "umami",
+        description: "Alternative to Google Analytics.",
+        link: "https://umami.is/",
+      },
+      {
+        name: "Product Hunt",
+        description: "Curation of the best new products.",
+        link: "https://www.producthunt.com/",
+      },
+      {
+        name: "Gumroad",
+        description: "E-commerce platform.",
+        link: "https://gumroad.com/",
+      },
+      {
+        name: "Futurepedia.io",
+        description: "AI tool directory.",
+        link: "https://www.futurepedia.io/",
+      },
+    ],
+  },
+  {
+    section: "Productivity",
+    data: [
+      {
+        name: "Excalidraw",
+        description: "Virtual collaborative whiteboard.",
+        link: "https://excalidraw.com/",
+      },
+      {
+        name: "Craft",
+        description: "Alternative to Notion.",
+        link: "https://www.craft.do/",
+      },
+    ],
+  },
+  {
+    section: "Front-End",
+    data: [
+      {
+        name: "React Icons",
+        description: "Popular icon packs.",
+        link: "https://react-icons.github.io/react-icons",
+      },
+      {
+        name: "heroicons",
+        description: "Icons from Tailwind CSS.",
+        link: "https://heroicons.com/",
+      },
+
+      {
+        name: "Flowbite",
+        description: "Components using Tailwind CSS.",
+        link: "https://flowbite.com/",
+      },
+      {
+        name: "Tailwind CSS",
+        description: "Utility-first CSS framework.",
+        link: "https://tailwindcss.com/",
+      },
+    ],
+  },
+]
