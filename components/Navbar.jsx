5b2454584e7ff94a4dfa9afcfd7d2c8c93f1100b
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-08 12:05:03 -0800

    Added background blur

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,6 +12,7 @@ export const Navbar = () => {
   const pathname = usePathname()
   return (
     <Popover className="absolute  mx-auto w-full px-2 sm:px-20">
+      <Popover.Overlay className="fixed inset-0 backdrop-blur-sm" />
       <div className="mx-2 px-2 md:mx-10">
         <div className="flex items-center justify-between py-4 md:justify-start md:space-x-10">
           <div className="flex flex-1 justify-start lg:w-0">
@@ -88,85 +89,99 @@ export const Navbar = () => {
           focus
           className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
         >
-          <div className="divide-y-2 divide-neutral-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-            <div className="px-5 pt-5 pb-6">
-              <div className="flex items-center justify-between">
-                <div>
-                  <Link href="/">
-                    <Image src="/dark-logo.png" alt="" width={35} height={35} />
-                  </Link>
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-transparent p-2 text-neutral-400 hover:bg-neutral-100 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Close menu</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-6 w-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
+          {({ close }) => (
+            <div className="divide-y-2 divide-neutral-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pt-5 pb-6">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <Link href="/">
+                      <Image
+                        src="/dark-logo.png"
+                        alt=""
+                        width={35}
+                        height={35}
                       />
-                    </svg>
-                  </Popover.Button>
+                    </Link>
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-transparent p-2 text-neutral-400 hover:bg-neutral-100 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Close menu</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </Popover.Button>
+                  </div>
+                </div>
+              </div>
+              <div className="space-y-6 py-6 px-5">
+                <div className="grid grid-cols-1 gap-y-4 gap-x-8">
+                  <Link
+                    className={
+                      pathname == "/about"
+                        ? "text-base font-bold text-neutral-500 hover:text-neutral-600"
+                        : "text-base text-neutral-500 hover:text-neutral-600"
+                    }
+                    href="/about"
+                    onClick={() => close()}
+                  >
+                    About
+                  </Link>
+
+                  <Link
+                    className={
+                      pathname == "/projects"
+                        ? "text-base font-bold text-neutral-500 hover:text-neutral-600"
+                        : "text-base text-neutral-500 hover:text-neutral-600"
+                    }
+                    href="/projects"
+                    onClick={() => close()}
+                  >
+                    Projects
+                  </Link>
+
+                  <Link
+                    className={
+                      pathname == "/resources"
+                        ? "text-base font-bold text-neutral-500 hover:text-neutral-600"
+                        : "text-base text-neutral-500 hover:text-neutral-600"
+                    }
+                    href="/resources"
+                    onClick={() => close()}
+                  >
+                    Resources
+                  </Link>
+                  <div>
+                    <ul className="flex flex-row items-center space-x-3">
+                      {SOCIALS_DARK.map((item, idx) => {
+                        return (
+                          <li key={idx}>
+                            <a
+                              href={item.link}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              {item.icon}
+                            </a>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="space-y-6 py-6 px-5">
-              <div className="grid grid-cols-1 gap-y-4 gap-x-8">
-                <Link
-                  className={
-                    pathname == "/about"
-                      ? "text-base font-bold text-neutral-500 hover:text-neutral-600"
-                      : "text-base text-neutral-500 hover:text-neutral-600"
-                  }
-                  href="/about"
-                >
-                  About
-                </Link>
-
-                <Link
-                  className={
-                    pathname == "/projects"
-                      ? "text-base font-bold text-neutral-500 hover:text-neutral-600"
-                      : "text-base text-neutral-500 hover:text-neutral-600"
-                  }
-                  href="/projects"
-                >
-                  Projects
-                </Link>
-
-                <Link
-                  className={
-                    pathname == "/resources"
-                      ? "text-base font-bold text-neutral-500 hover:text-neutral-600"
-                      : "text-base text-neutral-500 hover:text-neutral-600"
-                  }
-                  href="/resources"
-                >
-                  Resources
-                </Link>
-                <div>
-                  <ul className="flex flex-row items-center space-x-3">
-                    {SOCIALS_DARK.map((item, idx) => {
-                      return (
-                        <li key={idx}>
-                          <a href={item.link} target="_blank" rel="noreferrer">
-                            {item.icon}
-                          </a>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+          )}
         </Popover.Panel>
       </Transition>
     </Popover>
