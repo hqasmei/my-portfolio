e2eee1cda960e615cd8cb9044ac25b8ed47e069b
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-07 19:44:04 -0800

    Updated Tailwind config file

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -29,13 +29,13 @@ export default function ResourcesPage() {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="currentColor"
                         class="block h-5 w-5 transition-all duration-300 group-open:rotate-180"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                         />
                       </svg>
