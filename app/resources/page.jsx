af77c4b5f5962d79bd25acaa85d54545eee815d7
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-07 21:15:46 -0800

    Reverted back to original accordian

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -15,40 +15,55 @@ export default function ResourcesPage() {
         </p>
 
         <div className="mx-auto max-w-full">
-          <div className="divide-y divide-gray-100">
+          <div className="space-y-6">
             {sortedArr.map((item, idx) => {
               const sortedSubArr = item.data.sort((a, b) =>
                 a.name.localeCompare(b.name)
               )
               return (
-                <details key={idx} className="group">
-                  <summary className="text-secondary-900 flex cursor-pointer list-none items-center justify-between py-4 text-2xl font-medium text-neutral-100">
+                <details
+                  key={idx}
+                  className="group rounded-xl bg-white shadow-[0_10px_100px_10px_rgba(0,0,0,0.05)]"
+                >
+                  <summary className="text-secondary-900 flex cursor-pointer list-none items-center justify-between px-6 py-4 text-lg font-medium">
                     {item.section}
                     <div className="text-secondary-500">
-                      <RxCaretDown
-                        size={30}
-                        className="block transition-all duration-300 group-open:rotate-180"
-                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="block h-5 w-5 transition-all duration-300 group-open:-rotate-90"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                        />
+                      </svg>
                     </div>
                   </summary>
-                  <div className="text-secondary-500 pb-4">
-                    {" "}
-                    {sortedSubArr.map((i, i_idx) => {
-                      return (
-                        <p className="text-neutral-200">
-                          {" "}
-                          <a
-                            href={i.link}
-                            target="_blank"
-                            className="font-medium underline hover:text-neutral-400"
-                          >
-                            {i.name}
-                          </a>
-                          {" - "}
-                          {i.description}
-                        </p>
-                      )
-                    })}
+                  <div className="text-secondary-500 px-6 pb-6">
+                    <ul className="ml-8 list-disc text-neutral-800 ">
+                      {sortedSubArr.map((i, i_idx) => {
+                        return (
+                          <li key={i_idx}>
+                            <p>
+                              <a
+                                href={i.link}
+                                target="_blank"
+                                className="font-medium underline hover:text-neutral-900"
+                              >
+                                {i.name}
+                              </a>
+                              {" - "}
+                              {i.description}
+                            </p>
+                          </li>
+                        )
+                      })}
+                    </ul>
                   </div>
                 </details>
               )
