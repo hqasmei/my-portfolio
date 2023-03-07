7efd5c9943b3142fe8d601235c040d0a406db89a
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-03-07 00:42:42 -0800

    Temp commented out mdx code

--- app/about/[slug]/page.tsx
+++ app/about/[slug]/page.tsx
@@ -1,22 +1,22 @@
-import { getPostBySlug } from "../../../lib/mdx"
+// import { getPostBySlug } from "../../../lib/mdx"
 
-const getPageContent = async (slug: string) => {
-  const { meta, content } = await getPostBySlug(slug)
-  return { meta, content }
-}
+// const getPageContent = async (slug: string) => {
+//   const { meta, content } = await getPostBySlug(slug)
+//   return { meta, content }
+// }
 
-const Page = async ({ params }: { params: { slug: string } }) => {
-  const { content } = await getPageContent(params.slug)
+// const Page = async ({ params }: { params: { slug: string } }) => {
+//   const { content } = await getPageContent(params.slug)
 
-  return (
-    <section className="py-24">
-      <main className="grid h-screen place-items-center">
-        <div className="flex flex-row space-x-2">
-          <div className="container py-4 prose">{content}</div>
-        </div>
-      </main>
-    </section>
-  )
-}
+//   return (
+//     <section className="py-24">
+//       <main className="grid h-screen place-items-center">
+//         <div className="flex flex-row space-x-2">
+//           <div className="container py-4 prose">{content}</div>
+//         </div>
+//       </main>
+//     </section>
+//   )
+// }
 
-export default Page
+// export default Page
--- lib/mdx.tsx
+++ lib/mdx.tsx
@@ -1,44 +1,45 @@
-import fs from "fs"
-import path from "path"
-import { compileMDX } from "next-mdx-remote/rsc"
+// import fs from "fs"
+// import path from "path"
+// import { compileMDX } from "next-mdx-remote/rsc"
 
-const rootDirectory = path.join(process.cwd(), "content")
+// const rootDirectory = path.join(process.cwd(), "content")
 
-type PostMeta = {
-  title: string
-  author: string
-  publishDate: string
-  slug: string
-}
+// type Post = {
+//   meta: PostMeta
+//   content: string
+// }
 
-type Post = {
-  meta: PostMeta
-  content: string
-}
+// type PostMeta = {
+//   title: string
+//   author: string
+//   publishDate: string
+//   slug: string
+// }
 
-export const getPostBySlug = async (slug: string): Promise<Post> => {
-  const realSlug = slug.replace(/\.mdx$/, "")
-  const filePath = path.join(rootDirectory, `${realSlug}.mdx`)
+// export const getPostBySlug = async (slug: string): Promise<Post> => {
+//   const realSlug = slug.replace(/\.mdx$/, "")
+//   const filePath = path.join(rootDirectory, `${realSlug}.mdx`)
 
-  const fileContent = fs.readFileSync(filePath, { encoding: "utf8" })
+//   const fileContent = fs.readFileSync(filePath, { encoding: "utf8" })
 
-  const { frontmatter, content } = await compileMDX({
-    source: fileContent,
-    options: { parseFrontmatter: true },
-  })
+//   const { frontmatter, content } = await compileMDX({
+//     source: fileContent,
+//     options: { parseFrontmatter: true },
+//   })
 
-  return { meta: { ...frontmatter, slug: realSlug }, content }
-}
+//   console.log(content)
+//   return { meta: { ...frontmatter, slug: realSlug }, content }
+// }
 
-export const getAllPostsMeta = async (): Promise<PostMeta[]> => {
-  const files = await fs.promises.readdir(rootDirectory)
+// export const getAllPostsMeta = async (): Promise<PostMeta[]> => {
+//   const files = await fs.promises.readdir(rootDirectory)
 
-  let posts: PostMeta[] = []
+//   let posts: PostMeta[] = []
 
-  for (const file of files) {
-    const { meta } = await getPostBySlug(file)
-    posts.push(meta)
-  }
+//   for (const file of files) {
+//     const { meta } = await getPostBySlug(file)
+//     posts.push(meta)
+//   }
 
-  return posts
-}
+//   return posts
+// }
