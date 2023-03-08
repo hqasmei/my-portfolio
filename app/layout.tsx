a5f6860370e70f6b3ccd2e3665e2d0612a229643
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-03-08 00:53:47 -0800

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css"
 import type { Metadata } from "next"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Hosna Qasmei",
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
