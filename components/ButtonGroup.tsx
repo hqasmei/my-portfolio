1da5253572ccce815cea8d4fc79b1f9e80b08ee5
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-03-24 18:26:09 -0700

    Update package.json to include aicommits

--- components/ButtonGroup.tsx
+++ components/ButtonGroup.tsx
@@ -1,30 +1,38 @@
-import React from "react"
+import React, { useState } from "react";
 
 interface ButtonGroupProps {
-  buttons: { [key: string]: number }
-  setFilter: (key: string) => void
+  buttons: { [key: string]: number };
+  setFilter: (key: string) => void;
 }
 
 const ButtonGroup: React.FC<ButtonGroupProps> = ({ buttons, setFilter }) => {
+  const [activeButton, setActiveButton] = useState("all");
+
   const handleClick = (key: string) => {
-    setFilter(key)
-  }
+    setFilter(key);
+    setActiveButton(key);
+  };
 
   return (
     <div className="max-w-3xl">
-      <div className="overflow-x-auto flex pb-4 md:pb-0">
-        <div className="flex-none space-x-4 mb-4">
+      <div className="flex overflow-x-auto pb-4 md:pb-0">
+        <div className="mb-4 flex-none space-x-4">
           {Object.keys(buttons).map((key) => (
             <button
               key={key}
               onClick={() => handleClick(key)}
-              className="px-4 py-2 bg-stone-800 border-2 border-stone-600 text-white rounded-lg shadow-lg hover:bg-stone-700"
+              className={`rounded-lg border-2 px-4 py-2 text-white shadow-lg hover:bg-stone-700 ${
+                activeButton === key
+                  ? "border-stone-700 bg-stone-700"
+                  : "border-stone-600 bg-stone-800"
+              }`}
+              // className="rounded-lg border-2 border-stone-600 bg-stone-800 px-4 py-2 text-white shadow-lg hover:bg-stone-700"
             >
-              <div className="flex flex-row space-x-2 items-center">
-                <span className="text-sm md:text-md">
+              <div className="flex flex-row items-center space-x-2">
+                <span className="md:text-md text-sm">
                   {key.charAt(0).toUpperCase() + key.slice(1)}{" "}
                 </span>
-                <span className="text-sm md:text-md">
+                <span className="md:text-md text-sm">
                   {" ("}
                   {buttons[key]}
                   {")"}
@@ -35,7 +43,7 @@ const ButtonGroup: React.FC<ButtonGroupProps> = ({ buttons, setFilter }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonGroup
+export default ButtonGroup;
